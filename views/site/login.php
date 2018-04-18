c1bc9f5d0ce554a5eb0d10411e02619d6cac2f6d
jackblackjack <nospamit@mail.ru>
2018-04-18 12:02:50 +0300

    Add password recovery functional

--- views/site/login.php
+++ views/site/login.php
@@ -24,13 +24,17 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]); ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'autocomplete' => false]) ?>
 
         <?= $form->field($model, 'password')->passwordInput() ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
+        
+        <div style="color:#999;margin:1em 0">
+            If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+        </div>
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
