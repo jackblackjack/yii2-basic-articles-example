56327add5cdeb8b7b90953e76a3ea3d5e9ae7ace
jackblackjack <nospamit@mail.ru>
2018-04-18 17:35:57 +0300

    Add to article table column

--- migrations/m180417_233122_create_article_table.php
+++ migrations/m180417_233122_create_article_table.php
@@ -12,14 +12,21 @@ class m180417_233122_create_article_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%article}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull(),
             'preview_data' => $this->string(),
             'full_data' => $this->string()->notNull(),
+            'is_active' => $this->integer(6)->notNull()->defaultValue(0),
             'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-        ]);
+            'updated_at' => $this->integer()->notNull()
+        ], $tableOptions);
     }
 
     /**
