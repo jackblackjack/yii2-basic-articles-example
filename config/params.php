dc795a49304708aace85462f93095f1aa016fe79
jackblackjack <nospamit@mail.ru>
2018-05-08 11:03:08 +0300

    Add pagination and pjax default settings

--- config/params.php
+++ config/params.php
@@ -4,5 +4,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'user.passwordResetTokenExpire' => 3600,
     'supportEmail' => 'robot@example.com',
-    'rbac.roleOnRegister' => 'user'
+    'rbac.roleOnRegister' => 'user',
+    'pjax.timeout.default' => 10000,
+    'pagination.perpage.begin' => [ 10 => 10],
+    'pagination.perpage.default' => [ 10 => 10, 20 => 20, 50 => 50 ]
 ];
